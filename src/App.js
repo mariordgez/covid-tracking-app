29152d4a23be64e0f7b113a17f5765bbd43e95bf
mariordgez <mariordgez@hotmail.com>
2021-10-13 12:54:17 -0700

    Add structure of old project to start new one

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MyProfile from './components/MyProfile';
+import Dragons from './components/Dragons';
+import Missions from './components/Missions';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/missions">
+          <Missions />
+        </Route>
+        <Route path="/dragons">
+          <Dragons />
+        </Route>
+        <Route path="/my-profile">
+          <MyProfile />
+        </Route>
+        <Route path="/">
+          <Rockets />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
