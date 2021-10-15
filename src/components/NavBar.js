355cc289a807fd8b3afee2e82c8cac07e0b7ef65
mariordgez <mariordgez@hotmail.com>
2021-10-15 08:11:12 -0700

    Made navbar closer to wireframe

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone, faCog } from '@fortawesome/free-solid-svg-icons';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-  const links = [
-    {
-      id: 1,
-      path: '/',
-      name: 'Home',
-    },
-    {
-      id: 2,
-      path: '/',
-      name: 'Countries',
-    },
-  ];
-  return (
-    <Navbar className="bg-blue-light" expand="lg">
-      <Container>
-        <Nav className="me-auto d-flex justify-content-between flex-row w-100">
-          {links.map((link) => (
-            <Nav.Item key={link.id}>
-              <NavLink className="fw-bold m-3 text-decoration-none text-white" exact to={link.path}>
-                {link.name}
-              </NavLink>
-            </Nav.Item>
-          ))}
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-};
+const NavBar = () => (
+  <Navbar className="bg-blue-light" expand="lg">
+    <Container>
+      <Nav className="me-auto d-flex justify-content-between flex-row w-100">
+        <Nav.Item>
+          <NavLink className="fw-bold m-3 text-decoration-none text-white" exact to="/">
+            Home
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item className="text-decoration-none text-white">Covid Tracking app</Nav.Item>
+        <Nav.Item>
+          <span className="text-decoration-none text-white">
+            <FontAwesomeIcon icon={faMicrophone} className="me-4" />
+            <FontAwesomeIcon icon={faCog} />
+          </span>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  </Navbar>
+);
 
 export default NavBar;
